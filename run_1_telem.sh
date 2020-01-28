734d95f4e4e53c6456d7d57639d5e4f16ce005e2
conorwalsh <conorwalsh98@gmail.com>
2020-01-28 15:18:25 +0000

    Initial Project Commit

---
+++ run_1_telem.sh
@@ -0,0 +1,1 @@
+/root/walshc/dpdk1911/dpdk/examples/qos_sched_custom/build/qos_sched -l 26,27,71 -w 0000:83:00.0 --telemetry -- --pfc "0,0,27,71" --cfg /root/walshc/dpdk1911/dpdk/examples/qos_sched_custom/profile.cfg --mst 26
