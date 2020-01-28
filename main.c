58566896783f9b095523d9355adf8a1f4ca94809
Conor Walsh <conorwalsh98@gmail.com>
2020-01-28 17:23:22 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
-/* SPDX-License-Identifier: BSD-3-Clause
- * Copyright(c) 2010-2014 Intel Corporation
+/*
+ * main.c
+ * Version: v19.11
+ *
+ * This is the main file for the project
+ *
+ * Usage:
+ *    1) Example run command: ./build/qos_sched -l 26,27,71 -w 0000:83:00.0 -- --pfc "0,0,27,71" --cfg profile.cfg --mst 26
+ * 
+ * Copyright (c) 2020 Conor Walsh
+ * qos_sched_custom is licensed under an MIT license (see included license file)
+ *
+ * This project was based on the DPDK qos_sched app and as per it's original
+ *    BSD-3 licence the original copyright and licence are maintained below
+ * ------------------------------------------
+ *  SPDX-License-Identifier: BSD-3-Clause
+ *  Copyright(c) 2010-2014 Intel Corporation
+ * ------------------------------------------
  */
 
 #include <unistd.h>
@@ -17,6 +33,7 @@
 #include <rte_sched.h>
 
 #include "main.h"
+#include "cycle.h"
 
 #define APP_MODE_NONE 0
 #define APP_RX_MODE   1
@@ -176,20 +193,82 @@ app_stat(void)
 		memcpy(&tx_stats[i], &stats, sizeof(stats));
 
 #if APP_COLLECT_STAT
-		printf("-------+------------+------------+\n");
-		printf("       |  received  |   dropped  |\n");
-		printf("-------+------------+------------+\n");
-		printf("  RX   | %10" PRIu64 " | %10" PRIu64 " |\n",
+		printf("+--------+------------+------------+\n");
+		printf("|        |  received  |   dropped  |\n");
+		printf("+--------+------------+------------+\n");
+		printf("|   RX   | %10" PRIu64 " | %10" PRIu64 " |\n",
 			flow->rx_thread.stat.nb_rx,
 			flow->rx_thread.stat.nb_drop);
-		printf("QOS+TX | %10" PRIu64 " | %10" PRIu64 " |   pps: %"PRIu64 " \n",
-			flow->wt_thread.stat.nb_rx,
-			flow->wt_thread.stat.nb_drop,
-			flow->wt_thread.stat.nb_rx - flow->wt_thread.stat.nb_drop);
-		printf("-------+------------+------------+\n");
+		printf("| QOS+TX | %10" PRIu64 " | %10" PRIu64 " |\n",
+		        flow->wt_thread.stat.nb_rx,
+		        flow->wt_thread.stat.nb_drop);
+		if(CYCLE_GET_DE_QOS()!=0){	
+			printf("+--------+------------+------------+\n");
+			printf("         Per Packet QOS Stats       \n");
+			printf("+--------+------------+------------+\n");
+			printf("|        |  Enqueue   |  Dequeue   |\n");
+			printf("+--------+------------+------------+\n");
+			printf("| Cycles | %10" PRIu64 " | %10" PRIu64 " |   Total: %" PRIu64 "\n",
+				CYCLE_GET_EN_QOS(),
+				CYCLE_GET_DE_QOS(),
+				(CYCLE_GET_EN_QOS()+CYCLE_GET_DE_QOS()));
+			printf("+--------+------------+------------+\n");
+			printf("|        |     RX     |     TX     |\n");
+			printf("+--------+------------+------------+\n");
+			printf("| Cycles | %10" PRIu64 " | %10" PRIu64 " |   Total: %" PRIu64 "\n",
+				CYCLE_GET_RX(),
+				CYCLE_GET_TX(),
+				(CYCLE_GET_RX()+CYCLE_GET_TX()));
+			printf("+--------+------------+------------+\n");
+			printf("|        |    RING    |    SEND    |\n");
+			printf("+--------+------------+------------+\n");
+			printf("| Cycles | %10" PRIu64 " | %10" PRIu64 " |\n",
+				CYCLE_GET_RING_QOS(),
+				CYCLE_GET_SEND_QOS());
+			printf("+--------+------------+------------+\n");
+			printf("|        |  RX TOTAL  |QOS+TX TOTAL|\n");
+			printf("+--------+------------+------------+\n");
+			printf("| Cycles | %10" PRIu64 " | %10" PRIu64 " |\n",
+				CYCLE_GET_RX_TOTAL(),
+				CYCLE_GET_TX_TOTAL());
+			printf("+--------+------------+------------+\n");
+			printf("          Per Call QOS Stats        \n");
+			printf("+--------+------------+------------+\n");
+			printf("|        |     RX     |   QOS+TX   |\n");
+			printf("+-------+------------+------------+\n");
+			printf("|Idle CPC| %10" PRIu64 " | %10" PRIu64 " |\n",
+				CYCLE_GET_RX_IDLE_CPC(),
+				CYCLE_GET_TX_IDLE_CPC());
+			printf("|Used CPC| %10" PRIu64 " | %10" PRIu64 " |\n",
+				CYCLE_GET_RX_USED_CPC(),
+				CYCLE_GET_TX_USED_CPC());
+			printf("|  PPC   | %10.1f | %10.1f |\n",
+				CYCLE_GET_RX_PPC(),
+				CYCLE_GET_TX_PPC());
+			printf("|  IPU   | %10.1f | %10.1f |\n",
+				CYCLE_GET_RX_IPU(),
+				CYCLE_GET_TX_IPU());
+			printf("+--------+------------+------------+\n");
+			printf("Used Dequeue calls: %ld\n",CYCLE_GET_TX_USED_CALL());
+			printf("Idle Dequeue calls: %ld\n",CYCLE_GET_TX_IDLE_CALL());
+		}
+		else{
+			printf("+--------+------------+------------+\n");
+	                printf("         Per Packet QOS Stats       \n");
+	                printf("+--------+------------+------------+\n");
+	                printf("|            DISABLED              |\n");
+	                printf("+--------+------------+------------+\n");
+		}
+		printf("Avg Pkts in QoS: %ld\n",CYCLE_GET_DELTA());
+		printf("Subs: %" PRIu32 "\n",port_params.n_pipes_per_subport);
 		printf("Avg Pkt Size: %dB\n",avgpktsz);
 		printf("PPS: %.3fMPPS\n", (flow->wt_thread.stat.nb_rx - flow->wt_thread.stat.nb_drop)/1000000.0);
 		printf("Throughput: %.2fGbps\n",((((flow->wt_thread.stat.nb_rx - flow->wt_thread.stat.nb_drop)*(avgpktsz+24))*8.0)/1000000000.0));
+		if(CYCLE_GET_DE_QOS()!=0){
+			printf("CPC = Cycles Per Call\n");
+			printf("PPC = Packets Per Call (only counts for used calls)\n");
+			printf("IPU = Idle calls Per Used call\n");
+		}
 		memset(&flow->rx_thread.stat, 0, sizeof(struct thread_stat));
 		memset(&flow->wt_thread.stat, 0, sizeof(struct thread_stat));
 #endif
