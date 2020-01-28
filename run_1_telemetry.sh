daa1f8ac249f592e8791cfd925f5cce0a9a1d291
Conor Walsh <conorwalsh98@gmail.com>
2020-01-28 16:02:48 +0000

    Update run_1_telemetry.sh

--- run_1_telemetry.sh
+++ run_1_telemetry.sh
@@ -1,1 +1,1 @@
-/root/walshc/dpdk/examples/qos_sched_custom/build/qos_sched -l 26,27,71 -w 0000:83:00.0 --telemetry -- --pfc "0,0,27,71" --cfg /root/walshc/dpdk/examples/qos_sched_custom/profile.cfg --mst 26
+./build/qos_sched -l 26,27,71 -w 0000:83:00.0 --telemetry -- --pfc "0,0,27,71" --cfg profile.cfg --mst 26
